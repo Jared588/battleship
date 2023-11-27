c7972462b0a942dfe89828296cc366cbb91015fa
Jared588 <jaredeprice@gmail.com>
2023-11-27 03:16:56 +0200

    Create Ship object with functions and properties

---
+++ src/ships.js
@@ -0,0 +1,22 @@
+function createShip(name, length, hits, sunk) {
+  return {
+    name,
+    length,
+    hits,
+    sunk,
+
+    hit () {
+      this.hits += 1;
+    },
+
+    isSunk() {
+      return this.hits === this.length;
+    },
+
+    displayInfo () {
+      console.log(
+        `${this.name} length:${this.length} hits:${this.hits} sunk:${this.sunk}`,
+      );
+    },
+  };
+}
